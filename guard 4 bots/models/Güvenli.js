bbeda37eba5ba6a4ddefdf0a9a316188c27bc502
recepefsanesi <66915947+recepefsanesi@users.noreply.github.com>
2022-03-03 15:08:31 +0300

    Delete guard 4 bots directory

--- guard 4 bots/models/Güvenli.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const Güvenli = new mongoose.Schema({
-    guildID: { type: String, default: "" },
-    Safe: Array,
-});
-
-module.exports = mongoose.model("Güvenli", Güvenli);
